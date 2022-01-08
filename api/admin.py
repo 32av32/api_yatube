e066ffd55d68fe5fc7db6ce5453a317e6d0870b3
Isa Amerkhanov <isa.amerkhanov@gmail.com>
2022-01-09 00:36:37 +0300

    Added Simple JWT token authorization. Added api.models Group, Follow models. In posts.admin registered all models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post, Group, Comment, Follow
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'pub_date', 'author', 'group')
+    empty_value_display = '-empty-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description', 'slug')
+    empty_value_display = '-empty-'
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'post', 'text', 'created')
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'user')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
--- api/models.py
+++ api/models.py
@@ -6,16 +6,35 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    text = models.TextField(verbose_name='Text')
+    pub_date = models.DateTimeField(verbose_name='Date of publish', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Author')
+    group = models.ForeignKey('Group', blank=True, null=True, verbose_name='Group', on_delete=models.CASCADE,
+                              related_name='posts')
 
     def __str__(self):
         return self.text
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    slug = models.SlugField(verbose_name='Slug', unique=True)
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+
+    def __str__(self):
+        return self.title
+
+
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    text = models.TextField()
-    created = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', verbose_name='Author')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Post')
+    text = models.TextField(verbose_name='Text')
+    created = models.DateTimeField(verbose_name='Date of create', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower', verbose_name='User')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following', verbose_name='Author')
+
+    class Meta:
+        unique_together = ['user', 'author']
